ca1d1c6a47f029274447fa3d926564737fc69c76
Sohan Kancherla <sohank@gmail.comm>
2023-11-01 11:12:09 -0700

    Completed Grayscale Filter

--- helpers.c
+++ helpers.c
@@ -1,8 +1,20 @@
 #include "helpers.h"
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
